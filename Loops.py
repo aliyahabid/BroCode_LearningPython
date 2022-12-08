36931fccf026db136b355608099bbf24095067a4
aliyahabid <abid.aliyahp@gmail.com>
2022-12-08 00:41:40 -0500

    Nested loops

--- Loops.py
+++ Loops.py
@@ -6,11 +6,11 @@ import time
 # While loop = a statement that will execute its block of code
 # as long as its condition remains true
 
-name = ""
-while len(name) == 0:
-    name = input("Enter your name: ")
+# name = ""
+# while len(name) == 0:
+#   name = input("Enter your name: ")
 
-print("Hello "+name)
+# print("Hello "+name)
 
 # Alternatively, you could write
 
@@ -40,4 +40,24 @@ for seconds in range(10,0,-1):
     print(seconds)
     time.sleep(1)
 print("Surprise!")
+
+# Nested loops
+
+# With nested loops, the "inner loop" will finish all of its iterations
+# before finishing one iteration of the "outer loop"
+# A common convention is to write "j" for index in inner loops.
+
+
+# The following example will create a rectangle formed from the symbol of your choice.
+# Columns = width and rows = height
+rows = int(input("How many rows? "))
+columns = int(input("How many columns? "))
+symbol = input ("Enter a symbol of your choice: ")
+
+# The second print statement is a new line once we exit the inner loop.
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
+
 
