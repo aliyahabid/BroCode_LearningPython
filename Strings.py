531cea183ca400e8ee6fc2d4bce1a87978ef0f61
aliyahabid <abid.aliyahp@gmail.com>
2022-12-13 02:56:28 -0500

    String formatting

--- Strings.py
+++ Strings.py
@@ -20,8 +20,8 @@ current_age = score = 20
 decade = score / 2
 print("You're " + str(current_age) + " years old right now and " + str(score) + " years is called a score, " +
       "which is twice the amount of time in a decade. A decade is " + str(int(decade)) + " years.\n")
-print("decade = score/2 creates a " + str(type(decade)) + """data type, which is why I typecasted the variable 
-        decade into an integer then a string.""")
+print("decade = score/2 creates a " + str(type(decade)) + """ data type, 
+which is why I typecasted the variable decade into an integer then a string.""")
 print('Otherwise, the variable would have printed as ' + str(decade) + ".")
 
 birth_year, disney_princess, DC_superhero = 2002, "Mulan", "Wonder Woman"
@@ -35,6 +35,54 @@ full_name = first_name + " " + last_name
 print("Your name has " + str(len(full_name) - 1) + " letters in it. ")
 print('The letter "I" is "' + str(first_name.find("i") + 1) + " letters into your first name and " +
       str(last_name.find("i") + 1) + " letters into your last name.\n")
+
+# String Formatting
+
+# The str.format() is an optional method that gives users more control when displaying output.
+# The curly braces or {} are format fields, which act as placeholders for a value or a variable--
+# they work in order. However, you can also use a positional argument by listing the index of
+# the values you want to insert inside the format field.
+
+animal = "cow"
+item = "moon"
+
+print("The {} jumped over the {}.".format(animal, "moon"))
+print("The {} made {} on the {}.".format(animal,"milk", item))
+print(" ")
+
+# Positional argument:
+print("The {2} couldn't eat {0} on the {1}.".format("grass", item, animal))
+print("{0} ran into another {0}, but one was an {1}.".format("Santa","imposter"))
+print(" ")
+
+# Keyword argument pair:
+print("The {animal} climbed the {plant}.".format(plant="tree", animal="cat"))
+print("The {animal} barked at the other {animal}.".format(animal="dog",scene="grass"))
+print(" ")
+
+text = "A {} and a {} walked into a bar."
+print(text.format("mechanic", "writer"))
+print(" ")
+
+username = "robokop"
+
+# You can insert a number into a format field to allocate a specific amount of space for the values
+# you'll insert. You can do that by writing the number after a colon in the curly braces.
+# You can add padding when formatting as seen in the following examples.
+
+print("Welcome user {}. Here is your dashboard.".format(username))
+print("Welcome user {:10}. Here is your dashboard.".format(username)) # Default: left alignment
+print("Welcome user {:<10}. Here is your dashboard.".format(username)) # Left alignment
+print("Welcome user {:>10}. Here is your dashboard.".format(username)) # Right alignment
+print("Welcome user {:^10}. Here is your dashboard.".format(username)) # Center alignment
+print(" ")
+
+# Keyword argument examples:
+print("Welcome user {username:10}. Here is your {screen}.".format(username="whigpartyman",screen="dashboard"))
+print("Welcome user {username:20}. Here is your {screen}.".format(username="whigpartyman",screen="dashboard"))
+
+# Positional argument example:
+print("Welcome user {0:^20}. Here is your {1}.".format("whigpartyman","dashboard"))
 
 
 
